9c0288313493d381000adc3f3cf0ef0fae9ec6b3
sirkudos <attahikechukwu@gmail.com>
2023-01-07 17:38:03 -0500

    header component completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,34 @@
 import React from "react";
-import { AutoComplete } from "@react-google-maps/api";
-import { AppBar, ToolBar, Typography, InputBase, Box } from "@material-ui/core";
+import { Autocomplete } from "@react-google-maps/api";
+import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import useStyles from "./styles";
 
 const Header = () => {
+  const classes = useStyles();
   return (
     <AppBar position="static">
-      <h1>Header</h1>
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Restaurant Locator
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h6" className={classes.title}>
+            Explore new places
+          </Typography>
+          {/* <Autocomplete> */}
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search ..."
+              classes={{ root: classes.inputRoot, input: classes.inputInput }}
+            />
+          </div>
+          {/* </Autocomplete> */}
+        </Box>
+      </Toolbar>
     </AppBar>
   );
 };
