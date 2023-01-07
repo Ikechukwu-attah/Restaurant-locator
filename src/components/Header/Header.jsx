f0faf4b24656abb3645d5521f9e2d252d403510d
sirkudos <attahikechukwu@gmail.com>
2023-01-07 16:50:06 -0500

    initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { AutoComplete } from "@react-google-maps/api";
+import { AppBar, ToolBar, Typography, InputBase, Box } from "@material-ui/core";
+
+const Header = () => {
+  return (
+    <AppBar position="static">
+      <h1>Header</h1>
+    </AppBar>
+  );
+};
+
+export default Header;
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Header from "./Header/Header";
+import Map from "./Map/Map";
+import PlaceDetails from "./PlaceDetails/PlaceDetails";
+import List from "./List/List";
+
+export { Header, Map, PlaceDetails, List };
